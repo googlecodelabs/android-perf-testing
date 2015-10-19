4d3515a2eadc2394730b2455635e7a9d676233af
Paul Rashidi <paulrashidi@google.com>
2015-10-19 13:52:35 -0700

    Commenting code out that will be developed during codelab.
    Adding in TODOs for easier navigation.
    
    Change-Id: I0f1dc4fcce937d9c58379cd5fb1f147c196e55eb

--- app/src/androidTest/java/com/google/android/perftesting/MainActivityTest.java
+++ app/src/androidTest/java/com/google/android/perftesting/MainActivityTest.java
@@ -16,11 +16,6 @@
 
 package com.google.android.perftesting;
 
-import com.google.android.perftesting.testrules.EnableLogcatDump;
-import com.google.android.perftesting.testrules.EnableNetStatsDump;
-import com.google.android.perftesting.testrules.EnablePostTestDumpsys;
-import com.google.android.perftesting.testrules.EnableTestTracing;
-import com.google.android.perftesting.common.PerfTest;
 
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -29,27 +24,14 @@ import android.test.suitebuilder.annotation.LargeTest;
 
 import org.junit.Before;
 import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.Timeout;
 import org.junit.runner.RunWith;
-
-import java.util.concurrent.TimeUnit;
-
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.clearText;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static android.support.test.espresso.action.ViewActions.typeText;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 /**
  * For a small sample on just the Espresso framework see https://goo.gl/GOUP47
  */
 @RunWith(AndroidJUnit4.class)
 @LargeTest
-@PerfTest
+// @PerfTest
 public class MainActivityTest {
 
     public static final String STRING_TO_BE_TYPED = "Espresso";
@@ -65,34 +47,37 @@ public class MainActivityTest {
      * the activity under test. To get a reference to the activity you can use
      * the {@link ActivityTestRule#getActivity()} method.
      */
-    @Rule
-    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
-            MainActivity.class);
+      // TODO(developer): Uncomment below member variable to add a test activity to this test class.
+//    @Rule
+//    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+//            MainActivity.class);
+//
+//    @Rule
+//    public Timeout globalTimeout= new Timeout(20, TimeUnit.SECONDS);
+//
+//    @Rule
+//    public EnableTestTracing mEnableTestTracing = new EnableTestTracing();
+//
+//    @Rule
+//    public EnablePostTestDumpsys mEnablePostTestDumpsys = new EnablePostTestDumpsys();
+//
+//    @Rule
+//    public EnableLogcatDump mEnableLogcatDump = new EnableLogcatDump();
+//
+//    @Rule
+//    public EnableNetStatsDump mEnableNetStatsDump = new EnableNetStatsDump();
+//
 
-    @Rule
-    public Timeout globalTimeout= new Timeout(20, TimeUnit.SECONDS);
-
-    @Rule
-    public EnableTestTracing mEnableTestTracing = new EnableTestTracing();
-
-    @Rule
-    public EnablePostTestDumpsys mEnablePostTestDumpsys = new EnablePostTestDumpsys();
-
-    @Rule
-    public EnableLogcatDump mEnableLogcatDump = new EnableLogcatDump();
-
-    @Rule
-    public EnableNetStatsDump mEnableNetStatsDump = new EnableNetStatsDump();
-
-    @Test
-    @PerfTest
-    public void changeTextSameActivityTest() throws InterruptedException {
-        // Type text and then press the button.
-        onView(withId(R.id.edit_text_view))
-                .perform(clearText(), typeText(STRING_TO_BE_TYPED), closeSoftKeyboard());
-        onView(withId(R.id.edit_text_button)).perform(click());
-
-        // Check that the text was changed.
-        onView(withId(R.id.display_text_view)).check(matches(withText(STRING_TO_BE_TYPED)));
-    }
+      // TODO(developer): Uncomment below test method to add a simple test to the project.
+//    @Test
+//    @PerfTest
+//    public void changeTextSameActivityTest() throws InterruptedException {
+//        // Type text and then press the button.
+//        onView(withId(R.id.edit_text_view))
+//                .perform(clearText(), typeText(STRING_TO_BE_TYPED), closeSoftKeyboard());
+//        onView(withId(R.id.edit_text_button)).perform(click());
+//
+//        // Check that the text was changed.
+//        onView(withId(R.id.display_text_view)).check(matches(withText(STRING_TO_BE_TYPED)));
+//    }
 }
--- app/src/androidTest/java/com/google/android/perftesting/SimpleListActivityTest.java
+++ app/src/androidTest/java/com/google/android/perftesting/SimpleListActivityTest.java
@@ -16,32 +16,21 @@
 
 package com.google.android.perftesting;
 
-import com.google.android.perftesting.testrules.EnableLogcatDump;
-import com.google.android.perftesting.testrules.EnableNetStatsDump;
-import com.google.android.perftesting.testrules.EnablePostTestDumpsys;
-import com.google.android.perftesting.testrules.EnableTestTracing;
-import com.google.android.perftesting.common.PerfTest;
-
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.suitebuilder.annotation.LargeTest;
-import android.widget.ListView;
 
 import org.junit.Before;
 import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.Timeout;
 import org.junit.runner.RunWith;
-
-import java.util.concurrent.TimeUnit;
 
 /**
  * For a small sample on just the Espresso framework see https://goo.gl/GOUP47
  */
 @RunWith(AndroidJUnit4.class)
 @LargeTest
-@PerfTest
+// @PerfTest
 public class SimpleListActivityTest {
     public static final int SCROLL_TIME_IN_MILLIS = 2000;
     public static final long MAX_ADAPTER_VIEW_PROCESSING_TIME_IN_MILLIS = 500;
@@ -57,45 +46,48 @@ public class SimpleListActivityTest {
      * the activity under test. To get a reference to the activity you can use
      * the {@link ActivityTestRule#getActivity()} method.
      */
-    @Rule
-    public ActivityTestRule<SimpleListActivity> mActivityRule = new ActivityTestRule<>(
-            SimpleListActivity.class);
+      // TODO(developer): Uncomment below member variable to add a test activity to this test class.
+//    @Rule
+//    public ActivityTestRule<SimpleListActivity> mActivityRule = new ActivityTestRule<>(
+//            SimpleListActivity.class);
+//
+//    @Rule
+//    public Timeout globalTimeout= new Timeout(
+//        SCROLL_TIME_IN_MILLIS + MAX_ADAPTER_VIEW_PROCESSING_TIME_IN_MILLIS, TimeUnit.MILLISECONDS);
+//
+//    @Rule
+//    public EnableTestTracing mEnableTestTracing = new EnableTestTracing();
+//
+//    @Rule
+//    public EnablePostTestDumpsys mEnablePostTestDumpsys = new EnablePostTestDumpsys();
+//
+//    @Rule
+//    public EnableLogcatDump mEnableLogcatDump = new EnableLogcatDump();
+//
+//    @Rule
+//    public EnableNetStatsDump mEnableNetStatsDump = new EnableNetStatsDump();
+//
 
-    @Rule
-    public Timeout globalTimeout= new Timeout(
-        SCROLL_TIME_IN_MILLIS + MAX_ADAPTER_VIEW_PROCESSING_TIME_IN_MILLIS, TimeUnit.MILLISECONDS);
-
-    @Rule
-    public EnableTestTracing mEnableTestTracing = new EnableTestTracing();
-
-    @Rule
-    public EnablePostTestDumpsys mEnablePostTestDumpsys = new EnablePostTestDumpsys();
-
-    @Rule
-    public EnableLogcatDump mEnableLogcatDump = new EnableLogcatDump();
-
-    @Rule
-    public EnableNetStatsDump mEnableNetStatsDump = new EnableNetStatsDump();
-
-    @Test
-    @PerfTest
-    public void scrollFullList() throws InterruptedException {
-        ListView listView = (ListView) mActivityRule.getActivity().findViewById(android.R.id.list);
-
-        // Get last position and offset for zero-indexed position tracking.
-        int lastPosition = listView.getAdapter().getCount() - 1;
-
-        // Espresso method of scrolling to the last item.
-        // onData(anything()).atPosition(lastPosition);
-
-        // Standard Android method of scrolling to the last position.
-        listView.smoothScrollToPositionFromTop(lastPosition, 0, SCROLL_TIME_IN_MILLIS);
-
-        // Scrolling is performed asynchronously so we need to periodically loop and detect if
-        // we're finished scrolling yet. This can be delayed by any work being done to display
-        // data items in the ListView.
-        while (listView.getLastVisiblePosition() != lastPosition) {
-            Thread.sleep(300);
-        }
-    }
+      // TODO(developer): Uncomment below test method to add a list scrolling test to the project.
+//    @Test
+//    @PerfTest
+//    public void scrollFullList() throws InterruptedException {
+//        ListView listView = (ListView) mActivityRule.getActivity().findViewById(android.R.id.list);
+//
+//        // Get last position and offset for zero-indexed position tracking.
+//        int lastPosition = listView.getAdapter().getCount() - 1;
+//
+//        // Espresso method of scrolling to the last item.
+//        // onData(anything()).atPosition(lastPosition);
+//
+//        // Standard Android method of scrolling to the last position.
+//        listView.smoothScrollToPositionFromTop(lastPosition, 0, SCROLL_TIME_IN_MILLIS);
+//
+//        // Scrolling is performed asynchronously so we need to periodically loop and detect if
+//        // we're finished scrolling yet. This can be delayed by any work being done to display
+//        // data items in the ListView.
+//        while (listView.getLastVisiblePosition() != lastPosition) {
+//            Thread.sleep(300);
+//        }
+//    }
 }
