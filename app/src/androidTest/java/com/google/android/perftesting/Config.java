0db6222cb50d1948be15cebc63f37ec6350cf4d2
KevinChang <d900139@gmail.com>
2016-08-11 12:47:00 +0800

    Add Config.java, modify packagename here.

---
+++ app/src/androidTest/java/com/google/android/perftesting/Config.java
@@ -0,0 +1,9 @@
+package com.google.android.perftesting;
+
+/**
+ * Created by kevinchang on 2016/8/11.
+ */
+public class Config {
+    //Fill up the application's packagename you want to test.
+    public String packagename = "com.skysoft.kkbox.android";
+}
