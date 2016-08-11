479657400170d36917e93988a493a6a6c846fe42
KevinChang <d900139@gmail.com>
2016-08-11 12:37:15 +0800

    Let EnablePostTestDumpsys.java get packagename from Config.java.

--- app/src/androidTest/java/com/google/android/perftesting/testrules/EnablePostTestDumpsys.java
+++ app/src/androidTest/java/com/google/android/perftesting/testrules/EnablePostTestDumpsys.java
@@ -18,6 +18,8 @@ package com.google.android.perftesting.testrules;
 
 import android.os.Trace;
 
+import com.google.android.perftesting.Config;
+
 import org.junit.rules.ExternalResource;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
@@ -40,7 +42,8 @@ import static com.google.android.perftesting.common.PerfTestingUtils.getTestFile
  * </pre>
  */
 public class EnablePostTestDumpsys extends ExternalResource {
-
+    public static Config config = new Config();
+    private static final String BASIC_SAMPLE_PACKAGE = config.packagename;
     private Logger logger = Logger.getLogger(EnablePostTestDumpsys.class.getName());
 
     private String mTestName;
@@ -60,7 +63,7 @@ public class EnablePostTestDumpsys extends ExternalResource {
             builder.command("dumpsys", "gfxinfo", "--reset",
                     // NOTE: Using the android app BuildConfig specifically.
                     //com.google.android.perftesting.BuildConfig.APPLICATION_ID);
-                    "com.skysoft.kkbox.android");
+                    BASIC_SAMPLE_PACKAGE);
             Process process = builder.start();
             process.waitFor();
         } catch (Exception exception) {
@@ -79,7 +82,7 @@ public class EnablePostTestDumpsys extends ExternalResource {
                 // TODO: If less than API level 23 we should remove framestats.
                 processBuilder.command("dumpsys", "gfxinfo",
                         // NOTE: Using the android app BuildConfig specifically.
-                        "com.skysoft.kkbox.android",
+                        BASIC_SAMPLE_PACKAGE,
                         "framestats");
                 processBuilder.redirectErrorStream();
                 Process process = processBuilder.start();
