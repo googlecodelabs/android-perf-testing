38d0d799134e33b67b1aa9863aad9dbeed3e1de7
KevinChang <d900139@gmail.com>
2016-08-15 12:05:06 +0800

    Change sec to nanosec, resolve parse problem.

--- app/src/androidTest/java/com/google/android/perftesting/testrules/GetExecutionTime.java
+++ app/src/androidTest/java/com/google/android/perftesting/testrules/GetExecutionTime.java
@@ -19,8 +19,6 @@ import static com.google.android.perftesting.common.PerfTestingUtils.getTestFile
 
 public class GetExecutionTime extends ExternalResource{
 
-    private static final String LOG_TAG="GetExecutionTime";
-
     private String mTestName;
     private String mTestClass;
     private long StartTime;
@@ -35,27 +33,22 @@ public class GetExecutionTime extends ExternalResource{
 
     @Override
     public void before() {
-        Log.w(LOG_TAG, "------GetExecutionTime before--------");
         try {
-            Log.w(LOG_TAG,"--------------start------------");
             StartTime = System.nanoTime();
 
 
         } catch (Exception exception) {
 
-
         }
     }
 
     public void after() {
-        Log.w(LOG_TAG, "------GetExecutionTime after--------");
 
 
         FileWriter fileWriter = null;
         BufferedReader bufferedReader = null;
 
         try {
-            Log.w(LOG_TAG,"--------------end------------");
             EndTime = System.nanoTime();
 
 
@@ -63,7 +56,7 @@ public class GetExecutionTime extends ExternalResource{
 
             long output = EndTime - StartTime;
 
-            String str = String.valueOf("Execution Time : "+(output/1000000000f) + " sec");
+            String str = String.valueOf("Execution Time : "+ output + " ns");
 
             fileWriter.append(str);
 
